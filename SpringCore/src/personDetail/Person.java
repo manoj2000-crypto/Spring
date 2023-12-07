3656046cd3ba71a680df40998cb6110c699d33da
Manoj Kale <manojkale2000@gmail.com>
2023-12-07 17:27:02 +0530

    Three files has been "Modified Person.java", "PersonDetailHandler.java", "PersonDetail.xml"

--- SpringCore/src/personDetail/Person.java
+++ SpringCore/src/personDetail/Person.java
@@ -2,14 +2,14 @@ package personDetail;
 
 public class Person {
 
+	//Attribute
 	private String personName;
-	private Address address;
 
-	Person()
-	{
+	// Person class constructor
+	Person() {
 		System.out.println("Person Constructor invoked.");
 	}
-	
+
 	public String getPersonName() {
 		return personName;
 	}
@@ -17,6 +17,9 @@ public class Person {
 	public void setPersonName(String personName) {
 		this.personName = personName;
 	}
+
+	// Creating object reference for Address Interface.
+	private Address address;
 
 	public Address getAddress() {
 		return address;
@@ -26,16 +29,19 @@ public class Person {
 		this.address = address;
 	}
 
-	private HomeAddress homeAddress;
-	private OfficeAddress officeAddress;
+	// Creating object reference for HomeAddress class.
+	private HomeAddress homeAddressObj;
 
 	public HomeAddress getHomeAddress() {
-		return homeAddress;
+		return homeAddressObj;
 	}
 
-	public void setHomeAddress(HomeAddress homeAddress) {
-		this.homeAddress = homeAddress;
+	public void setHomeAddress(HomeAddress homeAddressObj) {
+		this.homeAddressObj = homeAddressObj;
 	}
+
+	// Creating object reference for OfficeAddress class.
+	private OfficeAddress officeAddress;
 
 	public OfficeAddress getOfficeAddress() {
 		return officeAddress;
--- SpringCore/src/personDetail/PersonDetailHandler.java
+++ SpringCore/src/personDetail/PersonDetailHandler.java
@@ -3,13 +3,22 @@ package personDetail;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+//This file is the Main for running the project.
 public class PersonDetailHandler {
 
 	public static void main(String[] args) {
+		// Creating IOC Container
 		ApplicationContext ioc = new ClassPathXmlApplicationContext("PersonDetail.xml");
-		
+
+		/*
+		 * We use getBean() because, we already created bean(Object) in
+		 * "PersonDetail.xml" file manually
+		 * 
+		 */
 		Person person = ioc.getBean("personObj", Person.class);
+
 		System.out.println("Name : " + person.getPersonName());
+
 		person.getHomeAddress().displayAddress();
 		person.getOfficeAddress().displayAddress();
 	}
